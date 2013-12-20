50fbf452fed8a7d465c107075ae78cb440cf5854
xiaofei1992 <shophy.cheng@gmail.com>
2013-12-20 18:54:23 +0800

    bundle install

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Interesting::Application.routes.draw do
-  #get "pages/home"
   
-     root "pages#home"
+root "pages#home"
+get "about" => "pages#about"
+	 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
